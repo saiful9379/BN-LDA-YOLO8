76b514ed68b2a7d52c3741ecf9f4182d62b2bc20
saiful <saifulbrur79@gmail.com>
2023-07-17 23:41:10 +0600

    training script update

--- train.py
+++ train.py
@@ -3,19 +3,11 @@ import os
 from ultralytics.yolo.engine.model import YOLO
 # from ultralytics  import YOLO
 
+model_name = "yolov8x-seg.pt"
+# model_name = "yolov8m-seg.pt"
+model_config = "./data/yolov8.yaml"
 
-class _config:
-    model_name = "./pretrained/yolov8m-seg.pt"
-    checkpoint = "./runs/segment/train28/weights/best.pt"
-    model_config = "./data/yolov8.yaml"
-    epoch = 300
-    img_dim = 1024
-    batch_size = 6
-    max_det = 600
-    resume=True
-
-cfg = _config()
-
+checkpoint = ""
 
 """
 # from checkpoint training need to use 
@@ -23,18 +15,19 @@ model = YOLO("last.pt")
 model.train(resume=True)
 """
 
-model_path = cfg.checkpoint if cfg.checkpoint else cfg.model_name
-resume = True if cfg.checkpoint else False
+model_path = checkpoint if checkpoint else model_name
+resume = True if checkpoint else False
 
 print("Resume : ", resume)
 print("CheckPoint path :" , model_path)
 
 model = YOLO(model_path)
 model.train(
-    data=cfg.model_config, 
-    epochs=cfg.epoch, 
-    imgsz=cfg.img_dim, 
-    batch=cfg.batch_size,
-    max_det = cfg.max_det,
-    resume = cfg.resume
+    data=model_config, 
+    epochs=600, 
+    imgsz= 1024, 
+    batch=4,
+    max_det=600,
+    resume=False,
+    optimizer="AdamW"
     )
